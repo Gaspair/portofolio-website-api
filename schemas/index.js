ec7e93c0653f388604af78d7c042301f59c170aa
Dragos <diulian16@gmail.com>
2023-03-26 11:51:31 +0000

    clean up

--- schemas/index.js
+++ schemas/index.js
@@ -1,19 +1,9 @@
-import testimonials from './testimonials'
 import skills from './skills'
 import works from './works'
 import contact from './contact'
 import workExperience from './workExperience'
 import experiences from './experiences'
-import brands from './brands'
+
 import abouts from './abouts'
 
-export const schemaTypes = [
-  testimonials,
-  skills,
-  works,
-  contact,
-  workExperience,
-  brands,
-  experiences,
-  abouts,
-]
+export const schemaTypes = [skills, works, contact, workExperience, experiences, abouts]
